f84cc76384876d80214837b9849685543cfbd2eb
PandeyAnkush <ankushapndey92@gmial.com>
2022-07-30 15:14:58 +0530

    First Commit of repo

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+//Given a list of strings and a string pattern. Check if the pattern is same for the words in the list.
+//abb ~ bcc , abb!~bcb  (122 ~ 122) , a will match all single size words.
+//Concepts Hash, List
+
+class Pattern{
+    public static void main(String arg[]){
+
+    }
+}
+class pat{
+    public String code(String st){
+        String code="";
+        return code;
+    }
+}
